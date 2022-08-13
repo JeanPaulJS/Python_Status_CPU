47710b68a98f117ee41f26688ea0ff5fe285dcde
Jean Paul <paulfromperu123@gmail.com>
2022-08-13 18:42:20 -0500

    There is a pyhton codes usings diferents libraries

---
+++ status_pc.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+from doctest import master
+import os
+import shutil
+import sys
+
+
+def check_reboot():
+    """ Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, Flase otherwise"""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate hoiw many free gigabytes there are
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+
+def check_root_full():
+    """Returns True is the root partition is full, False otherwise"""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+
+def main():
+    if check_reboot():
+        print("Pending reboot. ")
+        sys.exit(1)
+    if check_root_full():
+        print("Root partition full")
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
+
+main()
+
