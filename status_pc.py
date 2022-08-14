09a5ca06388c92d642aaa802a3405f9484c8fbca
Jean Paul <paulfromperu123@gmail.com>
2022-08-13 20:24:35 -0500

    Add changins to master with -Disk full- code

--- status_pc.py
+++ status_pc.py
@@ -22,17 +22,12 @@ def check_disk_full(disk, min_gb, min_percent):
     return False
 
 
-def check_root_full():
-    """Returns True is the root partition is full, False otherwise"""
-    return check_disk_full(disk="/", min_gb=2, min_percent=10)
-
-
 def main():
     if check_reboot():
         print("Pending reboot. ")
         sys.exit(1)
-    if check_root_full():
-        print("Root partition full")
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print("Disk full")
         sys.exit(1)
 
     print("Everything ok.")
